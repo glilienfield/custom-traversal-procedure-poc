9b944754c367cc86dbb481fc4595a9cfa66c7b9a
Gary Lilienfield <glilienfield@yahoo.com>
2022-05-12 13:39:24 -0400

    Refactored the unit tests to use a common method

--- src/test/java/CustomFunctionsTest.java
+++ src/test/java/CustomFunctionsTest.java
@@ -29,141 +29,81 @@ class CustomFunctionsTest {
     @Test
     @DisplayName("a has null list, b has null list")
     void test_both_nodes_have_null_lists() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1}), (b{id: 2})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertFalse(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1}), (b{id: 2})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertFalse(isSimilar);
     }
 
     @Test
     @DisplayName("a has null list, b has non-null list")
     void test_a_node_has_null_lists_b_node_has_non_null_list() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1}), (b{id: 2, list_extern: ['1','2']})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertFalse(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1}), (b{id: 2, list_extern: ['1','2']})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertFalse(isSimilar);
     }
 
     @Test
     @DisplayName("a has non-null list, b has null list")
     void test_a_node_has_non_null_lists_b_node_has_null_list() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: ['1','2']}), (b{id: 2})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertFalse(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: ['1','2']}), (b{id: 2})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertFalse(isSimilar);
     }
 
     @Test
     @DisplayName("a and b have lists of integers with no common elements")
     void test_a_and_b_have_lists_with_no_common_elements() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: ['10','20']}), (b{id: 2, list_extern: ['1','2']})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertFalse(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: ['10','20']}), (b{id: 2, list_extern: ['1','2']})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertFalse(isSimilar);
     }
 
     @Test
     @DisplayName("a and b have lists of integers with one common element")
     void test_a_and_b_have_lists_with_one_common_element() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: ['10','2']}), (b{id: 2, list_extern: ['1','2']})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertTrue(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: ['10','2']}), (b{id: 2, list_extern: ['1','2']})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertTrue(isSimilar);
     }
 
     @Test
     @DisplayName("a and b have lists of integers with multiple common element")
     void test_a_and_b_have_lists_with_multiple_common_element() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: ['10','2', '100']}), (b{id: 2, list_extern: ['100', '1','2']})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertTrue(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: ['10','2', '100']}), (b{id: 2, list_extern: ['100','1','2']})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertTrue(isSimilar);
     }
 
     @Test
     @DisplayName("a has a single element and b has a list that contains the single element in a")
     void test_a_has_a_single_element_and_b_has_a_list_that_contains_the_element_of_a() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: ['100', '1','2']})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertTrue(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: ['100','1','2']})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertTrue(isSimilar);
     }
 
     @Test
     @DisplayName("b has a single element and a has a list that contains the single element in b")
     void test_b_has_a_single_element_and_a_has_a_list_that_contains_the_element_of_b() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: ['100', '1','2']}), (b{id: 2, list_extern: '2'})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertTrue(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: ['100','1','2']}), (b{id: 2, list_extern: '2'})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertTrue(isSimilar);
     }
 
     @Test
     @DisplayName("a has a single element and b has the single element in b")
     void test_a_has_a_single_element_and_b_has_the_single_element_of_b() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: '2'})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertTrue(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: '2'})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertTrue(isSimilar);
     }
 
     @Test
     @DisplayName("a and b have single elements that are not equal")
     void test_a_and_b_have_a_single_element_that_are_not_equal() {
-        try (Session session = driver.session()) {
-            session.run("create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: '4'})");
-            Result result = session.run("match (a{id: 1}), (b{id: 2}) return com.glilienfield.isSimilar('list_intern', a, 'list_extern', b) as result");
-            if (result.hasNext()) {
-                Record record = result.next();
-                boolean isSimilar = record.get("result").asBoolean();
-                Assertions.assertFalse(isSimilar);
-            }
-        }
+        String cypher = "create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: '4'})";
+        boolean isSimilar = executeTest(cypher);
+        Assertions.assertFalse(isSimilar);
     }
 
     private boolean executeTest(String setupCypher) {
@@ -177,13 +117,5 @@ class CustomFunctionsTest {
                 throw new IllegalArgumentException();
             }
         }
-    }
-
-    @Test
-    @DisplayName("a and b have single elements that are not equal")
-    void test_method() {
-        String cypher = "create (a{id: 1, list_intern: '2'}), (b{id: 2, list_extern: '4'})";
-        boolean isSimilar = executeTest(cypher);
-        Assertions.assertFalse(isSimilar);
     }
 }
